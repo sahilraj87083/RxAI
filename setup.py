fd2d4c507f7fdc6722f8bcc4d60f2ee4709a4f9c
sahilraj87083 <sahilraj87093@gmail.com>
2025-10-28 16:43:20 +0530

    Project structure setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'RxAI',
+    version= '0.0.0',
+    author= 'Sahil Singh',
+    author_email= 'sahilraj87093@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
